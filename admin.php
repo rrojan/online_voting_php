7ef85caa4c16a2f2972a91f68ee52f6536578f37
Rojan Rajbhandari <rrojan20@gmail.com>
2022-08-13 16:00:02 +0545

    fix: heading

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@ if (!isset($_COOKIE['user']) || $_COOKIE['user'] != 'admin') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Portal</title>
 </head>
 <body>
     <a href="create-election.php">Create New Election</a>
